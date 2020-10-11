a0da4fc7129e12d9467bd27b0d7190ab7d1355a3
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-10 22:09:54 -0500

    Building the Lexer II.

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -1,6 +1,7 @@
 from re import match
 
 from lpp.token import (
+    lookup_token_type,
     Token,
     TokenType,
 )
@@ -16,6 +17,8 @@ class Lexer:
         self._read_character()
 
     def next_token(self) -> Token:
+        self._skip_whitespace()
+
         if match(r'^=$', self._character):
             token = Token(TokenType.ASSIGN, self._character)
         elif match(r'^\+$', self._character):
@@ -34,12 +37,27 @@ class Lexer:
             token = Token(TokenType.COMMA, self._character)
         elif match(r'^;$', self._character):
             token = Token(TokenType.SEMICOLON, self._character)
+        elif self._is_letter(self._character):
+            literal = self._read_identifier()
+            token_type = lookup_token_type(literal)
+
+            return Token(token_type, literal)
+        elif self._is_number(self._character):
+            literal = self._read_number()
+
+            return Token(TokenType.INT, literal)
         else:
             token = Token(TokenType.ILLEGAL, self._character)
 
         self._read_character()
 
         return token
+
+    def _is_letter(self, character: str) -> bool:
+        return bool(match(r'^[a-záéíóúA-ZÁÉÍÓÚñÑ_]$', character))
+
+    def _is_number(self, character: str) -> bool:
+        return bool(match(r'^\d$', character))
 
     def _read_character(self) -> None:
         if self._read_position >= len(self._source):
@@ -49,4 +67,25 @@ class Lexer:
 
         self._position = self._read_position
         self._read_position += 1
+
+    def _read_identifier(self) -> str:
+        initial_position = self._position
+
+        while self._is_letter(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+
+    def _read_number(self) -> str:
+        initial_position = self._position
+
+        while self._is_number(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+
+    def _skip_whitespace(self) -> None:
+        while match(r'^\s$', self._character):
+            self._read_character()
+
 
--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -75,3 +75,58 @@ class LexerTest(TestCase):
 
         self.assertEquals(tokens, expected_tokens)
 
+    def test_assignment(self) -> None:
+        source: str = 'variable cinco = 5;'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(5):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'cinco'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.INT, '5'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_function_declaration(self) -> None:
+        source: str = '''
+            variable suma = procedimiento(x, y) {
+                x + y;
+            };
+        '''
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(16):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'suma'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.FUNCTION, 'procedimiento'),
+            Token(TokenType.LPAREN, '('),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.RPAREN, ')'),
+            Token(TokenType.LBRACE, '{'),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.PLUS, '+'),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.SEMICOLON, ';'),
+            Token(TokenType.RBRACE, '}'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_function_call(self) -> None:
+        self.fail('Not implemented yet')
+        source: str = 'variable resultado = suma(dos, tres);'
+
