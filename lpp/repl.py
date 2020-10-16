ea8020c51c5738433ac8ddd1d6c92f179d5be976
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-15 22:55:12 -0500

    Improve repl

--- lpp/repl.py
+++ lpp/repl.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from lpp.ast import Program
 from lpp.lexer import Lexer
+from lpp.parser import Parser
 from lpp.token import (
     Token,
     TokenType
@@ -8,10 +12,22 @@ from lpp.token import (
 EOF_TOKEN: Token = Token(TokenType.EOF, '')
 
 
+def _print_parse_errors(errors: List[str]):
+    for error in errors:
+        print(error)
+
+
 def start_repl() -> None:
     while (source := input('>> ')) != 'salir()':
         lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
 
-        while (token := lexer.next_token()) != EOF_TOKEN:
-            print(token)
+        program: Program = parser.parse_program()
+
+        if len(parser.errors) > 0:
+            _print_parse_errors(parser.errors)
+            continue
+
+        print(program)
+
 
