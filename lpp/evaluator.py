4cdde2daff3988ccd5f02888e7ce8df34766433a
David Aroesti <josedavid.aroesti@gmail.com>
2020-11-07 17:51:55 -0500

    Evaluate integer infix expressions

--- lpp/evaluator.py
+++ lpp/evaluator.py
@@ -1,7 +1,18 @@
-from typing import cast, List, Optional
+from typing import (
+    cast,
+    List,
+    Optional,
+    Type,
+)
 
 import lpp.ast as ast
-from lpp.object import Boolean, Integer, Null, Object
+from lpp.object import (
+    Boolean,
+    Integer,
+    Null,
+    Object,
+    ObjectType,
+)
 
 
 TRUE = Boolean(True)
@@ -10,7 +21,7 @@ NULL = Null()
 
 
 def evaluate(node: ast.ASTNode) -> Optional[Object]:
-    node_type = type(node)
+    node_type: Type = type(node)
 
     if node_type == ast.Program:
         node = cast(ast.Program, node)
@@ -39,6 +50,15 @@ def evaluate(node: ast.ASTNode) -> Optional[Object]:
 
         assert right is not None
         return _evaluate_prefix_expression(node.operator, right)
+    elif node_type == ast.Infix:
+        node = cast(ast.Infix, node)
+
+        assert node.left is not None and node.right is not None
+        left = evaluate(node.left)
+        right = evaluate(node.right)
+
+        assert right is not None and left is not None
+        return _evaluate_infix_expression(node.operator, left, right)
 
     return None
 
@@ -63,6 +83,34 @@ def _evaluate_bang_operator_expression(right: Object) -> Object:
         return FALSE
 
 
+def _evaluate_infix_expression(operator: str, 
+                                left: Object, 
+                                right: Object) -> Object:
+    if left.type() == ObjectType.INTEGER \
+            and right.type() == ObjectType.INTEGER:
+        return _evaluate_integer_infix_expression(operator, left, right)
+    else:
+        return NULL
+
+
+def _evaluate_integer_infix_expression(operator: str,
+                                       left: Object,
+                                       right: Object) -> Object:
+    left_value: int = cast(Integer, left).value
+    right_value: int = cast(Integer, right).value
+
+    if operator == '+':
+        return Integer(left_value + right_value)
+    elif operator == '-':
+        return Integer(left_value - right_value)
+    elif operator == '*':
+        return Integer(left_value * right_value)
+    elif operator == '/':
+        return Integer(left_value // right_value)
+    else:
+        return NULL
+
+
 def _evaluate_minus_operator_expression(right: Object) -> Object:
     if type(right) != Integer:
         return NULL
@@ -70,6 +118,7 @@ def _evaluate_minus_operator_expression(right: Object) -> Object:
     right = cast(Integer, right)
 
     return Integer(-right.value)
+
 
 def _evaluate_prefix_expression(operator: str, right: Object) -> Object:
     if operator == '!':
